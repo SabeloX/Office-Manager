aa2f04ec31a594df93b7906e8f4b2cc33a9b9624
SabeloX <sabelomtw@gmail.com>
2024-05-29 18:36:21 +0200

    OM-03: add the office details section and show when 'more' is clicked

--- components/office-block/OfficeBlock.tsx
+++ components/office-block/OfficeBlock.tsx
@@ -1,9 +1,14 @@
 import { StaffIcon } from "@/icons/StaffIcon";
-import { OfficeDetails } from "../../interfaces/office-details";
+import { OfficeDetails } from "../../interfaces/office-details.interface";
 
 import './office-block.css';
 import { ArrowDown } from "@/icons/ArrowDown";
 import { EditIcon } from "@/icons/EditIcon";
+import { PhoneIcon } from "@/icons/PhoneIcon";
+import { EmailIcon } from "@/icons/EmailIcon";
+import { CapacityIcon } from "@/icons/CapacityIcon";
+import { AddressIcon } from "@/icons/AddressIcon";
+import { useState } from "react";
 
 interface Props {
     name: string;
@@ -12,9 +17,21 @@ interface Props {
 }
 
 export const OfficeBlock = ({ name, totalStaff, details }: Props) => {
+    const [viewMore, setViewMore] = useState<boolean>(false);
+
     return (
-        <div className="office-block">
-            <span className="office-block__sidebar"></span>
+        <div
+            className="office-block"
+            style={{
+                height: viewMore ? 320 : 142
+            }}
+        >
+            <span 
+                className="office-block__sidebar"
+                style={{
+                    backgroundColor: details.color
+                }}
+            ></span>
             <div className="office-block__info">
                 <h3>{ name }</h3>
                 <EditIcon className="office-block__edit" />
@@ -22,11 +39,37 @@ export const OfficeBlock = ({ name, totalStaff, details }: Props) => {
                     <StaffIcon />
                     <p><span>{ totalStaff }</span> Staff Members in Office</p>
                 </div>
-                <div>
+                <div onClick={() => setViewMore(viewMore => !viewMore)}>
                     <p>More</p>
-                    <ArrowDown />
+                    <ArrowDown className={`office-block__arrow-${viewMore ? 'up' : 'down'}`} />
                 </div>
+                <OfficeBlockDetails details={details} />
             </div>
         </div>
-    )
+    );
 }
+
+interface OfficeDetailsProps {
+    details: OfficeDetails;
+}
+
+const OfficeBlockDetails = ({ details }: OfficeDetailsProps ) => (
+    <ul className="office-block__details">
+        <li>
+            <PhoneIcon />
+            <p>{details.contactNumber}</p>
+        </li>
+        <li>
+            <EmailIcon />
+            <p>{details.email}</p>
+        </li>
+        <li>
+            <CapacityIcon />
+            <p>Office Capacity: {details.capacity}</p>
+        </li>
+        <li>
+            <AddressIcon />
+            <p>{details.address}</p>
+        </li>
+    </ul>
+);
---
+++ interfaces/colors.interface.ts
@@ -0,0 +1,13 @@
+export enum Colors {
+    COLOR_1 = '#FFBE0B',
+    COLOR_2 = '#FF9B71',
+    COLOR_3 = '#FB5607',
+    COLOR_4 = '#97512C',
+    COLOR_5 = '#DBBADD',
+    COLOR_6 = '#FF006E',
+    COLOR_7 = '#A9F0D1',
+    COLOR_8 = '#00B402',
+    COLOR_9 = '#489DDA',
+    COLOR_10 = '#0072E8',
+    COLOR_11 = '#8338EC'
+}
