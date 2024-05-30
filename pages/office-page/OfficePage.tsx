33f05c092933fae35725005a75d643340f9ba11b
SabeloX <sabelomtw@gmail.com>
2024-05-30 19:15:07 +0200

    OM-09: add modal to add new staff

--- pages/office-page/OfficePage.tsx
+++ pages/office-page/OfficePage.tsx
@@ -12,6 +12,7 @@ import { StaffList } from '@/components/staff-list/StaffList';
 import { Addbutton } from '@/components/add-button/AddButton';
 import { useState } from 'react';
 import { Modal } from '@/components/modal/Modal';
+import { CloseIcon } from '@/icons/CloseIcon';
 
 export const OfficePage = () => {
     const [cookies] = useCookies<'currentOffice', { currentOffice: Office }>(['currentOffice']);
@@ -30,7 +31,15 @@ export const OfficePage = () => {
             <Search onSearch={handleSearch}/>
             <StaffList staffList={staffResults} />
             <Addbutton onClick={() => setOpen(true)} />
-            <Modal open={open} onClose={() => setOpen(false)} />
+            <Modal open={open} onClose={() => setOpen(false)}>
+                <div>
+                    <h3>New Staff Member</h3>
+                    <CloseIcon />
+                </div>
+                <form>
+                    
+                </form>
+            </Modal>
         </div>
     );
 }
