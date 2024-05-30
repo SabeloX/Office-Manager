6128a1611bb0a361c8c726121e9cd042f0f20d3c
SabeloX <sabelomtw@gmail.com>
2024-05-30 20:31:07 +0200

    OM-09: add the first step - staff member names

--- pages/office-page/OfficePage.tsx
+++ pages/office-page/OfficePage.tsx
@@ -10,7 +10,7 @@ import { Office, Staff } from '@/interfaces';
 import { Search } from '@/components/search/Search';
 import { StaffList } from '@/components/staff-list/StaffList';
 import { Addbutton } from '@/components/add-button/AddButton';
-import { useState } from 'react';
+import { Dispatch, ReactNode, SetStateAction, useState } from 'react';
 import { Modal } from '@/components/modal/Modal';
 import { CloseIcon } from '@/icons/CloseIcon';
 import { InputField } from '@/components/input-field/InputField';
@@ -23,9 +23,20 @@ export const OfficePage = () => {
     const [open, setOpen] = useState<boolean>(false);
     const [step, setStep] = useState<number>(0);
 
+
     const handleSearch = (results: string) => {
         const filteredStaff = cookies.currentOffice.staff.filter(staffMember => `${staffMember.firstName} ${staffMember.lastName}`.toLowerCase().includes(results));
         setStaffResults(filteredStaff);
+    }
+
+    const Steps = (): ReactNode => {
+        switch(step){
+            default:
+            case 0:
+                return <StepOne step={step} setStep={setStep} />;
+            case 1:
+                return <StepTwo />;
+        }
     }
 
     return (
@@ -40,13 +51,57 @@ export const OfficePage = () => {
                     <h3>New Staff Member</h3>
                     <CloseIcon onClick={() => setOpen(false)} />
                 </div>
-                <form>
-                    <InputField placeholder='First name'/>
-                    <InputField placeholder='Last name'/>
-                    <Steps numberOfSteps={2} activeStep={step} />
-                    <Button onClick={() => {}}>Next</Button>
-                </form>
+                { Steps() }
             </Modal>
         </div>
+    );
+}
+
+interface StepOneProps {
+    step: number;
+    setStep: Dispatch<SetStateAction<number>>;
+}
+
+const StepOne = ({ step, setStep }: StepOneProps) => {
+    const [firstName, setFirstName] = useState<string>('');
+    const [lastName, setLastName] = useState<string>('');
+    const [validationError, setValidationError] = useState<boolean>(false);
+
+    const handleNextStep = () => {
+        if(firstName !== '' && lastName !== ''){
+            setValidationError(false);
+            setStep(currentStep => currentStep + 1);
+        }
+        else{
+            setValidationError(true);
+        }
+    }
+
+    return (
+    <>
+        <form>
+            <div>
+                <InputField 
+                    placeholder='First name'
+                    value={firstName}
+                    onChange={(value) => setFirstName(value)}
+                />
+                <InputField
+                    placeholder='Last name'
+                    value={lastName}
+                    onChange={(value) => setLastName(value)}
+                />
+                <small>{ validationError && 'Enter both fields to proceed.' }</small>
+            </div>
+            <Steps numberOfSteps={2} activeStep={step} />
+            <Button onClick={handleNextStep}>Next</Button>
+        </form>
+    </>
+    );
+}
+
+const StepTwo = () => {
+    return (
+        <></>
     );
 }
